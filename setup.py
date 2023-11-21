68880461f8eaf46ead7c76f415083a089b962e42
Guillermo Alcantara Gonzalez <GuimoAAGG@gmail.com>
2023-11-21 16:29:24 -0600

    Structure
    Initial code commit and project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Removes the white-space of images.',
+    author='PepsiCo-IAI-COE-CV',
+    license='BSD-3',
+)
